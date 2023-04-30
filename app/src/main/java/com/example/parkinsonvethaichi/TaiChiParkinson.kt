4667b0460abbae2a43801a0d63b2a25460957ad7
CSS17 <ofucer99@gmail.com>
2023-04-30 14:05:49 +0300

    -TaiChiParkinson.kt and activity_tai_chi_parkinson.xml updated with sound button's effect. -Each media may play at the same time.But user can see which medias are playing.

--- app/src/main/java/com/example/parkinsonvethaichi/TaiChiParkinson.kt
+++ app/src/main/java/com/example/parkinsonvethaichi/TaiChiParkinson.kt
@@ -1,10 +1,13 @@
 package com.example.parkinsonvethaichi
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,14 +17,80 @@ import java.io.*
 
 class TaiChiParkinson : AppCompatActivity() {
 
-
-
+    private lateinit var mediaPlayer1: MediaPlayer
+    private lateinit var mediaPlayer2: MediaPlayer
+    private lateinit var mediaPlayer3: MediaPlayer
+    private lateinit var mediaPlayer4: MediaPlayer
+    private lateinit var mediaPlayer5: MediaPlayer
+    private lateinit var mediaPlayer6: MediaPlayer
+    private lateinit var mediaPlayer7: MediaPlayer
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tai_chi_parkinson)
+
+        mediaPlayer1 = MediaPlayer.create(this, R.raw.record)
+        mediaPlayer2 = MediaPlayer.create(this, R.raw.record2)
+        mediaPlayer3 = MediaPlayer.create(this, R.raw.record3)
+        mediaPlayer4 = MediaPlayer.create(this, R.raw.record4)
+        mediaPlayer5 = MediaPlayer.create(this, R.raw.record5)
+        mediaPlayer6 = MediaPlayer.create(this, R.raw.record6)
+        mediaPlayer7 = MediaPlayer.create(this, R.raw.record7)
+        val mediaPlayers= arrayOf(mediaPlayer1,mediaPlayer2,mediaPlayer3,mediaPlayer4,mediaPlayer5,mediaPlayer6,mediaPlayer7)
+
+        fun stopMedia(){
+            for(i in mediaPlayers){
+                if(i.isPlaying){
+                    i.stop()
+                    i.prepare()
+                }
+            }
+        }
+
+
+
+        fun playMusic(mediaPlayer:MediaPlayer,voiceButton: ImageButton,view:View){
+            if(mediaPlayer.isPlaying){
+                mediaPlayer.stop()
+                mediaPlayer.prepare()
+                voiceButton.setImageResource(R.drawable.volume_base)
+            }
+            else{
+                mediaPlayer.start()
+                voiceButton.setImageResource(R.drawable.volume)
+            }
+            mediaPlayer.setOnCompletionListener {
+                voiceButton.setImageResource(R.drawable.volume_base)
+            }
+        }
+
+
+        fun voiceClick(view: View){
+            //stopMedia()
+            when (view.id) {
+                R.id.ses_butonu_1 -> playMusic(mediaPlayer1,ses_butonu_1, view)
+                R.id.ses_butonu_2 -> playMusic(mediaPlayer2,ses_butonu_2, view)
+                R.id.ses_butonu_3 -> playMusic(mediaPlayer3,ses_butonu_3, view)
+                R.id.ses_butonu_4 -> playMusic(mediaPlayer4,ses_butonu_4, view)
+                R.id.ses_butonu_5 -> playMusic(mediaPlayer5,ses_butonu_5, view)
+                R.id.ses_butonu_6 -> playMusic(mediaPlayer6,ses_butonu_6, view)
+                R.id.ses_butonu_7 -> playMusic(mediaPlayer7,ses_butonu_7, view)
+            }
+
+        }
+        ses_butonu_1.setOnClickListener(::voiceClick)
+        ses_butonu_2.setOnClickListener(::voiceClick)
+        ses_butonu_3.setOnClickListener(::voiceClick)
+        ses_butonu_4.setOnClickListener(::voiceClick)
+        ses_butonu_5.setOnClickListener(::voiceClick)
+        ses_butonu_6.setOnClickListener(::voiceClick)
+        ses_butonu_7.setOnClickListener(::voiceClick)
+
+
+
+
         // Metin butonuna tıklanınca
         ParkinsonTitle.setOnClickListener {
             // Metin butonu tıklandığında görünürlük özelliğini değiştireceğiz.
@@ -32,6 +101,7 @@ class TaiChiParkinson : AppCompatActivity() {
                 ParkinsonInfo.visibility = View.VISIBLE
             }
         }
+
 
          ParkinsonReasonsTitle.setOnClickListener {
              // Metin butonu tıklandığında görünürlük özelliğini değiştireceğiz.
@@ -82,6 +152,16 @@ class TaiChiParkinson : AppCompatActivity() {
             }
         }
 
+    }
+    override fun onDestroy() {
+        mediaPlayer1.release()
+        mediaPlayer2.release()
+        mediaPlayer3.release()
+        mediaPlayer4.release()
+        mediaPlayer5.release()
+        mediaPlayer6.release()
+        mediaPlayer7.release()
+        super.onDestroy()
     }
 
     }
